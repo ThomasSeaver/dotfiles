3b1b4ec8f1ed3e660d658be7755f25e918753c44
Thomas Seaver <tseaver@spear.ai>
2025-07-10 11:43:12 -0400

    Finalization stuff

--- private_dot_config/private_fish/conf.d/fnm.fish
+++ private_dot_config/private_fish/conf.d/fnm.fish
@@ -1,1 +1,7 @@
 fnm env --use-on-cd --shell fish | source
+# fnm
+set FNM_PATH "/Users/thomasseaver/Library/Application Support/fnm"
+if [ -d "$FNM_PATH" ]
+  set PATH "$FNM_PATH" $PATH
+  fnm env | source
+end
